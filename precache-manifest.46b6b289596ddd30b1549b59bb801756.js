1c69d14ace430d796cb8f58b338e9d0c356e8819
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-14 01:20:14 +0000

    Updates

---
+++ precache-manifest.46b6b289596ddd30b1549b59bb801756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b836f751bc24452f60a152e6ce328c",
+    "url": "/fcc-calculator/index.html"
+  },
+  {
+    "revision": "f8ebd4dd43ff13eb3a81",
+    "url": "/fcc-calculator/static/css/main.d8b774aa.chunk.css"
+  },
+  {
+    "revision": "75c3338c7eec49e388fd",
+    "url": "/fcc-calculator/static/js/2.a1d083aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-calculator/static/js/2.a1d083aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8ebd4dd43ff13eb3a81",
+    "url": "/fcc-calculator/static/js/main.ad2a4e14.chunk.js"
+  },
+  {
+    "revision": "7050c44170c2d4ace14e",
+    "url": "/fcc-calculator/static/js/runtime-main.eda8ea2b.js"
+  }
+]);
